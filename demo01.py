262cab4888de2f37701b1150d3a44d93099872f1
anan <anship12@126.com>
2020-03-25 09:46:52 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,104 @@
+'''
+print("hello world!")
+print(2333)   #整数
+print(2.333)   #小数
+print(True)   #布尔值
+print(())    #元组
+print([])   #数组
+print({})   #字典
+print("哈哈",2333,2.333)
+print("哈哈"+"嘻嘻")
+print("哈哈"*100)
+print(1+2*3)
+print(2>1)
+name = "张三" 
+print(name)
+
+明日花绮罗
+明日花绮罗
+'''
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# print("a+b=",a+b)
+
+# a = len(input('请输入：'))
+# b = len(input('请输入：'))
+# print('两段内容的长度之和',a+b)
+
+#元祖 ,下标,从0开始编号
+# a = ("mi","霉霉","a妹",6,6,6,6,6,1,4,5,"阿呆")
+# # print(a[1])
+
+# #切片
+# print(a[0:3])    #左闭右开
+# print(a[3:7])
+# print(a[7:])
+
+# # print(a.index("a妹"))    查看值的下标
+# # print(a.count(6))      统计有多少个值
+
+# #二维元组
+# # b = (a,"戳爷","jb")
+# # print(b[0][1])
+
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+# print(a.index("mi"))
+# print(a.count(6))
+a = ["mi","霉霉","a妹",6,6,6,6,6,1,4,5,"阿呆"]
+#元祖一旦写好后不可以修改,而数组是可以修改的
+# a.append('霉霉1')
+# a.append('霉霉2')
+# a.insert(0,'霉霉3')
+# print(a)
+
+# b = a.pop(0) #剪切
+# c = a.pop(0) #剪切
+# print(b+c)
+
+# a.clear()
+
+xx = ['你好','不好']
+# a.extend(xx)
+print(a+xx)
+
+# b = a.remove('阿呆')
+# print(b)
+# print(a)
+
+# a = {'name':'霉霉','sex':'女','age':30}
+# #取值
+# print(a['name'])
+# #新增
+# a['high'] = '180cm'
+# print(a)
+# #修改
+# a['name'] = '霉霉霉'
+# print(a)
+#获取值
+# b=a.get('name')
+# print(b)
+#剪切
+# b=a.pop('name')
+# print(b)
+# print(a)
+#更新
+# a.update(name=1111)
+# print(a)
+
+# print(a.get('name11'))
+# print(a['name11'])
+
+
+# name = (input('name：'))
+# age = (input('age：'))
+# sex = (input('sex：'))
+# a = {'name':name,'age':age,'sex':sex}
+# print(a)
+
